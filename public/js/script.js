dfb776abe51b4aa31980d3c1450d6c1ce27ede6e
General Assembly | Anthony Ng <generalassemblyanthonyng@Anthonys-MacBook-Air.local>
2015-11-20 12:19:01 -0500

    Set up script and css files

---
+++ public/js/script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+let socket = io();
+let myUser;
+
+$(function() {
+
+  // Login button
+  $('#login-input').keypress((event) => {
+    if(event.keyCode === 13) {
+      let username = $('#login-input').val();
+      myUser = username;
+      socket.emit('add user', username);
+      $('#login-input').val('');
+      $('#loginpage').hide();
+    }
+  });
+
+  // Message button
+  $('#message').keypress(function(event) {
+    if(event.keyCode === 13) {
+      let message = $('#message').val();
+      socket.emit('send message', {name: myUser, message: message});
+      $('#message').val('');
+    }
+  });
+
+});
+
+// ===========================================================================
+// Socket Events =============================================================
+// ===========================================================================
+
+socket.on('user joined', (users) => {
+    let usersList = $('#users ul');
+    usersList.empty();
+    // update user list
+    users.forEach((user) => {
+      let userElement = $('<li>');
+      userElement.text(user.name);
+      usersList.append(userElement);
+    });
+});
+
+socket.on('send message', (data) => {
+  // update chat messages
+  let chatList = $('#messages');
+  let message = $('<li>');
+  message.text(data.name + ' : ' + data.message);
+  chatList.append(message);
+});
